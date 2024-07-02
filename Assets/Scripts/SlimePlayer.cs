a7b179f9704c51e3571d06e3f016a0527af72894
Yamm23 <zidanchamlingrai@gmail.com>
2024-07-02 09:01:44 +0545

    SlimeMovement

--- Assets/Scripts/SlimePlayer.cs
+++ Assets/Scripts/SlimePlayer.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class SlimePlayer : MonoBehaviour
 {
-    [SerializeField] Rigidbody slimeRigidBody;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D myrigidBody;
+    public float speed = 5.0f;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        myrigidBody.MovePosition(myrigidBody.position+movement*speed*Time.fixedDeltaTime);
     }
 }
