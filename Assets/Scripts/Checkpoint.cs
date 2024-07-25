3399f066c324cbccfbc311f519ea3710012d602e
Yamm23 <zidanchamlingrai@gmail.com>
2024-07-25 11:46:30 +0545

    CheckPointSystem
    Integrated checkpoint system in the game.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private RespawnBoundary respawn;
+    private BoxCollider2D checkpointCollider;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        checkpointCollider = GetComponent<BoxCollider2D>();
+        respawn = GameObject.FindGameObjectWithTag("Respawn").GetComponent<RespawnBoundary>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            respawn.respawnPoint = this.gameObject;
+            checkpointCollider.enabled = false;
+        }
+    }
+}
